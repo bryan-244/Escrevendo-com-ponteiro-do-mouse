efb541d05c52c5aa6fd347ca34508bf4c1a6a407
bryan-244 <bryan.cezarino@escola.pr.gov.br>
2024-11-01 09:04:08 -0300

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,1 +1,14 @@
 
+function setup() {
+  createCanvas(1550, 800);
+   background("green");
+}
+
+function draw() {
+  if (mouseIsPressed) {
+    fill("green");
+  } else {
+    fill(255);
+  }
+  ellipse(mouseX, mouseY, 20, 20);
+}
